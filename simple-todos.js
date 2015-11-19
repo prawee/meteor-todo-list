83ef06b61a3cbd923559bfa85d14a3e1d2ac4495
Prawee Wongsa <prawee@hotmail.com>
2015-11-19 10:31:18 +0700

    init

---
+++ simple-todos.js
@@ -0,0 +1,33 @@
+Tasks = new Mongo.Collection("tasks");
+
+if (Meteor.isClient) {
+    Template.body.helpers({
+//        tasks:[
+//            {text:"This is task 1"},
+//            {text:"This is task 2"},
+//            {text:"This is task 3"}
+//        ]
+        tasks:function(){
+            return Tasks.find({},{sort:{createdAt:-1}});
+        }
+    });
+
+    Template.body.events({
+        "submit .new-task":function(event){
+            event.preventDefault();
+            var text=event.target.text.value;
+            Tasks.insert({
+                text:text,
+                createdAt:new Date()
+            });
+
+            event.target.text.value="";
+        }
+    });
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
