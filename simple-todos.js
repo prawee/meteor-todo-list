359b70d723cacdcd211ecf2c28df2e381f9a474d
Prawee Wongsa <prawee@hotmail.com>
2015-11-20 13:42:12 +0700

    define addTask(),deleteTask(),setChecked method

--- simple-todos.js
+++ simple-todos.js
@@ -65,6 +65,27 @@ if (Meteor.isClient) {
     Accounts.ui.config({
         passwordSignupFields:"USERNAME_ONLY"
     });
+
+    Meteor.methods({
+        addTask:function(text){
+            if(!Meteor.userId()){
+                throw new Meteor.Error('not-authorized');
+            }
+
+            Tasks.insert({
+                text:text,
+                createdAt:new Date(),
+                owner:Meteor.userId(),
+                username:Meteor.user().username
+            });
+        },
+        deleteTask:function(taskId){
+            Tasks.remove(taskId);
+        },
+        setChecked:function(taskId,setChecked){
+            Tasks.update(taskId,{$set:{checked:setChecked}});
+        }
+    });
 }
 
 if (Meteor.isServer) {
